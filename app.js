e3971d8556e21c2380ec2c8d380dba8eb14484cf
Preston Weber <preston159@gmail.com>
2019-09-23 17:59:20 -0700

    first push

---
+++ app.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $("#search").on("click", function {
+        
+        var searchQuery = $("#searchInput").val();
+        console.log(searchQuery);
+    });
+});
