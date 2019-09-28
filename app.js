e11cf5e52e56190f528234a580744067374528d9
Preston Weber <preston159@gmail.com>
2019-09-27 18:52:07 -0700

    fixed javascript link, got api pulling data, and appending results to the page

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-$(document).ready(function() {
-    $("#search").on("click", function {
-        
-        var searchQuery = $("#searchInput").val();
+$(document).ready(function () {
+    console.log("test")
+    $("#search").on("click", function (event){
+        event.preventDefault();
+        console.log("string");
+        var searchQuery = $("#searchInput").val().trim();
         console.log(searchQuery);
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=bhqvXaF8TfOmk3JWmZfTYsVi7Jj6XYGW&q=" + searchQuery +"&limit=25&offset=0&rating=PG&lang=en"
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response.data);
+            for(i = 0; i < response.data.length; i++) {
+                var source = response.data[i].images.original.url;
+            var imageTag = $("<img>").attr("src", source);
+            $("#results").append(imageTag);
+            }
+            
+            
+            
+        });
     });
 });
