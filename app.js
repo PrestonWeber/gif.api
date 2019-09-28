24430c94284ca694a1b35d8b9ebd6908e2469f36
Preston Weber <preston159@gmail.com>
2019-09-27 19:58:54 -0700

    added the ability to utilize search to create buttons, then used buttons to search gifs

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 $(document).ready(function () {
-    console.log("test")
-    $("#search").on("click", function (event){
+    $("#search").on("click", function (event) {
         event.preventDefault();
-        console.log("string");
+
         var searchQuery = $("#searchInput").val().trim();
-        console.log(searchQuery);
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=bhqvXaF8TfOmk3JWmZfTYsVi7Jj6XYGW&q=" + searchQuery +"&limit=25&offset=0&rating=PG&lang=en"
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response.data);
-            for(i = 0; i < response.data.length; i++) {
-                var source = response.data[i].images.original.url;
-            var imageTag = $("<img>").attr("src", source);
-            $("#results").append(imageTag);
-            }
+        var newButtonClass = "button";
+        var newButton = $("<button>");
+        newButton.attr("class", newButtonClass);
+        newButton.text(searchQuery);
+        $("#newButton").append(newButton);
+
+        newButton.on("click", function (event) {
+            event.preventDefault();
+            console.log(searchQuery);
             
-            
-            
+            var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=bhqvXaF8TfOmk3JWmZfTYsVi7Jj6XYGW&q=" + searchQuery + "&limit=25&offset=0&rating=PG&lang=en"
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+                console.log(response.data);
+                for (i = 0; i < response.data.length; i++) {
+                    var source = response.data[i].images.original.url;
+                    var imageTag = $("<img>").attr("src", source);
+                    $("#results").prepend(imageTag);
+                }
+
+
+
+            });
         });
     });
 });
